3e5ac45a5321f19d3f4dea5a4fc86e3e65e09bdc
ShadyShade74 <shadyshade74@gmail.com>
2025-12-05 23:04:46 +0100

    duzo zmian

--- js/classes.js
+++ js/classes.js
@@ -48,12 +48,64 @@ class Enemy {
     c.arc(this.center.x ,this.center.y ,this.radius ,0 , Math.PI * 2)
     c.fill()
 
-    //health bar
     c.fillStyle= 'red'
     c.fillRect(this.position.x , this.position.y - 15, this.width , 9)
 
     c.fillStyle= 'rgba(39, 199, 216, 1)'
     c.fillRect(this.position.x , this.position.y - 15, this.width * this.health / 100, 9)
+
+  }
+
+  update() {
+    this.draw()
+
+    const waypoint = waypoints[this.waypointIndex]
+    const yDistance = waypoint.y - this.center.y
+    const xDistance = waypoint.x - this.center.x
+    const angle = Math.atan2(yDistance, xDistance)
+    this.position.x += Math.cos(angle)
+    this.position.y += Math.sin(angle)
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+
+    if (
+      Math.round(this.center.x) === Math.round(waypoint.x) &&
+      Math.round(this.center.y) === Math.round(waypoint.y) &&
+      this.waypointIndex < waypoints.length - 1
+    ) {
+      this.waypointIndex++
+    }
+  }
+}
+class Wolf {
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position
+    this.width = 70
+    this.height = 70
+    this.waypointIndex = 0
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+    this.radius = 25
+    this.health = 50
+    this.armor = 0.7
+  }
+
+  draw() {
+    c.fillStyle = 'gray'
+    c.beginPath()
+    c.arc(this.center.x ,this.center.y ,this.radius ,0 , Math.PI * 2)
+    c.fill()
+
+    //health bar
+    c.fillStyle= 'red'
+    c.fillRect(this.position.x , this.position.y - 15, this.width , 9)
+
+    c.fillStyle= 'rgba(73, 196, 210, 1)'
+    c.fillRect(this.position.x , this.position.y - 15, this.width * this.health / 50, 9)
 
   }
 
--- js/script.js
+++ js/script.js
@@ -11,11 +11,13 @@ const waveDisplay = document.querySelector('.wave-display')
 function WaveUpdate(){
   waveDisplay.textContent = `Wave: ${waveCounter} / ${win}`
 }
+const pop_up = document.querySelector('.pop_up')
 const placementTilesData2D = []
 
 for (let i = 0; i < placementTilesData.length; i += 20) {
   placementTilesData2D.push(placementTilesData.slice(i, i + 20))
 }
+
 const tileImage = new Image()
 tileImage.src = 'media/Free-Spot.png'
 const placementTiles = []
@@ -35,13 +37,12 @@ placementTilesData2D.forEach((row, y) => {
   })
 })
 
-
-
 const image = new Image()
 
 image.onload = () => {
   animate()
 }
+
 image.src = 'media/map.png'
 
 const enemies = []
@@ -54,71 +55,115 @@ function spawnEnemies(enemyCount){
         position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
       })
     )
-    
+  }
+}
+
+function spawnEnemies2(enemyCount2){
+  for (let i = 1; i < enemyCount2 + 1; i++) {
+    const xOffset = i * 200
+    enemies.push(
+      new Wolf({
+        position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
+      })
+    )
   }
 }
 
 const buildings = []
-let enemyCount =3
-let xp =0
-let waveCounter =1
+let enemyCount = 3
+let enemyCount2 = 6
+let xp = 0
+let waveCounter = 1
 let activeTile = undefined
 let win = 40
 let winCounter = 0
+
+
+
 spawnEnemies(enemyCount)
 
+
+
 function animate() {
-    requestAnimationFrame(animate)
-  
-    c.drawImage(image, 0, 0)
-    for(let i = enemies.length - 1; i >= 0; i--){
-      const enemy = enemies[i]
-      enemy.update()
-    }
-    placementTiles.forEach((tile) => {
-      tile.update(mouse)
+  requestAnimationFrame(animate)
+
+  c.drawImage(image, 0, 0)
+
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i]
+    enemy.update()
+  }
+
+  placementTiles.forEach((tile) => {
+    tile.update(mouse)
+  })
+
+  buildings.forEach((building) => {
+    building.update()
+    building.target = null
+
+    const validEnemies = enemies.filter(enemy =>{
+      const xDifference = enemy.center.x - building.center.x
+      const yDifference = enemy.center.y - building.center.y
+      const distance = Math.hypot(xDifference , yDifference)
+      return distance < enemy.radius + building.radius 
     })
 
-    buildings.forEach((building) => {
-      building.update()
-      building.target = null
+    building.target = validEnemies[0]
 
-      const validEnemies = enemies.filter(enemy =>{
-        const xDifference = enemy.center.x - building.center.x
-        const yDifference = enemy.center.y - building.center.y
-        const distance = Math.hypot(xDifference , yDifference)
-        return distance < enemy.radius + building.radius 
-      })
-      building.target = validEnemies[0]
-      for(let i = building.projectiles.length - 1; i >= 0; i--){
-        const projectile = building.projectiles[i]
-      
-        projectile.update()
+    for(let i = building.projectiles.length - 1; i >= 0; i--){
+      const projectile = building.projectiles[i]
 
-        const xDifference = projectile.enemy.center.x - projectile.position.x
-        const yDifference = projectile.enemy.center.y - projectile.position.y
-        const distance = Math.hypot(xDifference , yDifference)
-        if(distance < projectile.enemy.radius + projectile.radius){
-          projectile.enemy.health -= projectile.damage
-          if (projectile.enemy.health <= 0){
-            const enemyIndex = enemies.findIndex((enemy) =>{
+      projectile.update()
 
-              return projectile.enemy === enemy
-            })
-            if(enemyIndex > -1)enemies.splice(enemyIndex , 1)
-            xp += 5
-          }
-            if(enemies.length === 0){
-              enemyCount += 2
-              spawnEnemies(enemyCount)
-              waveCounter += 1
-              winCounter += 1
-            }
-          building.projectiles.splice(i, 1)
+      const xDifference = projectile.enemy.center.x - projectile.position.x
+      const yDifference = projectile.enemy.center.y - projectile.position.y
+      const distance = Math.hypot(xDifference , yDifference)
+
+      if(distance < projectile.enemy.radius + projectile.radius){
+
+        projectile.enemy.health -= projectile.damage
+
+        if (projectile.enemy.health <= 0){
+          const enemyIndex = enemies.findIndex((enemy) =>{
+            return projectile.enemy === enemy
+          })
+
+          if(enemyIndex > -1) enemies.splice(enemyIndex , 1)
+
+          xp += 5
         }
+
+        building.projectiles.splice(i, 1)
       }
-    })
-    WaveUpdate()
+    }
+  })
+
+
+
+
+  if(enemies.length === 0){
+
+    enemyCount += 2
+    spawnEnemies(enemyCount)
+
+    waveCounter += 1
+    winCounter += 1
+
+    if (waveCounter === 10) {
+      pop_up.style.display = "block"
+    } else {
+      pop_up.style.display = "none"
+    }
+
+
+    if (waveCounter >= 10) {
+      enemyCount2 += 3
+      spawnEnemies2(enemyCount2)
+    }
+  }
+
+  WaveUpdate()
 }
 
 const mouse = {
@@ -138,16 +183,14 @@ canvas.addEventListener('click', (event) => {
     )
     activeTile.isOccupied = true
   }
-
 })
 
 window.addEventListener('mousemove', (event) => {
   const rect = canvas.getBoundingClientRect()
 
-    mouse.x = event.clientX - rect.left
-    mouse.y = event.clientY - rect.top
+  mouse.x = event.clientX - rect.left
+  mouse.y = event.clientY - rect.top
 
-  
   activeTile = null
   for (let i = 0; i < placementTiles.length; i++) {
     const tile = placementTiles[i]
