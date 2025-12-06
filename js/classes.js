a798e341ca6f4694d5948820a112fc781de0b140
ShadyShade74 <shadyshade74@gmail.com>
2025-12-06 23:23:55 +0100

    .

--- js/classes.js
+++ js/classes.js
@@ -91,7 +91,134 @@ class Enemy {
     }
   }
 }
+class Wolf {
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position
+    this.width = 70
+    this.height = 70
+    this.waypointIndex = 0
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+    this.radius = 35
+    this.health = 100
+    this.armor = 1
+    this.velocity = {
+      x:0,
+      y:0
+    }
+    this.speed = 3
+  }
 
+  draw() {
+    c.fillStyle = 'red'
+    c.beginPath()
+    c.arc(this.center.x ,this.center.y ,this.radius ,0 , Math.PI * 2)
+    c.fill()
+
+    //health bar
+    c.fillStyle= 'red'
+    c.fillRect(this.position.x , this.position.y - 15, this.width , 9)
+
+    c.fillStyle= 'rgba(39, 199, 216, 1)'
+    c.fillRect(this.position.x , this.position.y - 15, this.width * this.health / 100, 9)
+
+  }
+
+  update() {
+    this.draw()
+
+    const waypoint = waypoints[this.waypointIndex]
+    const yDistance = waypoint.y - this.center.y
+    const xDistance = waypoint.x - this.center.x
+    const angle = Math.atan2(yDistance, xDistance)
+
+    this.velocity.x = Math.cos(angle)* this.speed
+    this.velocity.y = Math.sin(angle)* this.speed
+    
+    this.position.x += this.velocity.x 
+    this.position.y += this.velocity.y 
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+
+    if (
+      Math.abs(Math.round(this.center.x) - Math.round(waypoint.x)) <
+       Math.abs(this.velocity.x )&&
+      Math.abs(Math.round(this.center.y) - Math.round(waypoint.y)) <
+      Math.abs(this.velocity.y )&&
+      this.waypointIndex < waypoints.length - 1
+    ) {
+      this.waypointIndex++
+    }
+  }
+}
+class Knight {
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position
+    this.width = 70
+    this.height = 70
+    this.waypointIndex = 0
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+    this.radius = 35
+    this.health = 100
+    this.armor = 1
+    this.velocity = {
+      x:0,
+      y:0
+    }
+    this.speed = 3
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.beginPath()
+    c.arc(this.center.x ,this.center.y ,this.radius ,0 , Math.PI * 2)
+    c.fill()
+
+    //health bar
+    c.fillStyle= 'red'
+    c.fillRect(this.position.x , this.position.y - 15, this.width , 9)
+
+    c.fillStyle= 'rgba(39, 199, 216, 1)'
+    c.fillRect(this.position.x , this.position.y - 15, this.width * this.health / 100, 9)
+
+  }
+
+  update() {
+    this.draw()
+
+    const waypoint = waypoints[this.waypointIndex]
+    const yDistance = waypoint.y - this.center.y
+    const xDistance = waypoint.x - this.center.x
+    const angle = Math.atan2(yDistance, xDistance)
+
+    this.velocity.x = Math.cos(angle)* this.speed
+    this.velocity.y = Math.sin(angle)* this.speed
+    
+    this.position.x += this.velocity.x 
+    this.position.y += this.velocity.y 
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2
+    }
+
+    if (
+      Math.abs(Math.round(this.center.x) - Math.round(waypoint.x)) <
+       Math.abs(this.velocity.x )&&
+      Math.abs(Math.round(this.center.y) - Math.round(waypoint.y)) <
+      Math.abs(this.velocity.y )&&
+      this.waypointIndex < waypoints.length - 1
+    ) {
+      this.waypointIndex++
+    }
+  }
+}
 
   
 class Building {
